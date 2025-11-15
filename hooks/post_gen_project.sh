a002a1e8d0413e8921d025e36699cc8b11e1aefb
Annalise Mariottini <annalise.mariottini@columbia.edu>
2025-11-15 15:19:16 -0500

    Add trunk to mise.toml and add config to skip project generation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,34 +2,48 @@
 
 set -e
 
-# Initialize git repository
-git init
-git add .
-git commit -m "Created project"
-
-# Initialize trunk
-trunk init -y
-
-git add .trunk
-git commit -m "Initialized trunk"
+print_step() {
+    echo "‍🍪 Cookiecutter ==> $1"
+}
 
 # Trust files
+print_step "Trusting files"
 mise trust -a
 
-# Install dependencies and generate project
-if ! which tuist > /dev/null; then
-    echo "Tuist is not installed. Would you like to install it? (y/n)"
-    read -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        mise install tuist
-    else
-        echo "Please install tuist manually to generate the project."
-        exit 1
-    fi
+# Install dependencies
+print_step "Installing dependencies"
+mise install
+
+# Initialize git repository
+print_step "Initializing git repository"
+git init
+git add .
+git commit -m "Created project" --no-verify
+
+# Initialize trunk
+print_step "Initializing trunk"
+trunk init -y
+trunk check enable swiftformat
+
+git add .trunk
+git commit -m "Initialized trunk" --no-verify
+
+# Format code
+print_step "Formatting code"
+trunk fmt --all
+if ! git diff --quiet; then
+    git add .
+    git commit -m "Formatted code" --no-verify
 fi
 
-tuist install
-tuist generate
+# Generate project
+if [ "{{ cookiecutter.generate_project }}" = "true" ]; then
+    print_step "Generating project"
+    tuist install
+    tuist generate
 
-git add .
-git commit -m "Generated project"
+    git add .
+    git commit -m "Generated project" --no-verify
+else
+    print_step "Skipping project generation"
+fi
